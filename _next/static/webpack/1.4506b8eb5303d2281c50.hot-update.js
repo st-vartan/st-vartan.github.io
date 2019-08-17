f11b59b6f0e14b834ef6e4e846d286d02e76d9b9
Yun Xing <ink.xing.yun@gmail.com>
2019-08-17 13:36:45 -0400

    revise

---
+++ _next/static/webpack/1.4506b8eb5303d2281c50.hot-update.js
@@ -0,0 +1,105 @@
+webpackHotUpdate(1,{
+
+/***/ "./components/home/sections/introStatic.js":
+/*!*************************************************!*\
+  !*** ./components/home/sections/introStatic.js ***!
+  \*************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_scroll_modules__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-scroll/modules */ "./node_modules/react-scroll/modules/index.js");
+/* harmony import */ var react_scroll_modules__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_scroll_modules__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/bigfatdog/open-source/st-vartan-website/components/home/sections/introStatic.js";
+
+
+
+var introduction = function introduction() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "home-section bg-dark bg-dark-alfa-70",
+    id: "home",
+    style: {
+      backgroundPosition: '50% 0px',
+      backgroundImage: 'url("/static/images/home/court_bg.jpg")'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "js-height-full container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "home-content",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "home-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "hs-line-1 home-title font-alt mb-80 mb-xs-30 mt-50 mt-sm-0",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, "St.Vartan Hoops"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hs-line-6 home-sub-title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, "When the sun goes down, basketball rises")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_scroll_modules__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+    to: 'about',
+    spy: true,
+    smooth: true,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "scroll-down",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+    className: "fa fa-angle-down scroll-down-icon",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    },
+    __self: this
+  })))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (introduction);
+
+/***/ })
+
+})
+//# sourceMappingURL=1.4506b8eb5303d2281c50.hot-update.js.map
